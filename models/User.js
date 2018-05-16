338d5b26580be42c89aeea25e663f743a8960d9d
harrisonbrock <harrison@harrisonbrock.com>
2018-05-16 16:57:38 -0400

    Create startup for project and added User Model

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        require:true,
+        minLength: 4,
+    },
+});
+
+// Hash password before saving to database
+userSchema.pre('save', function (next) {
+
+    return bcrypt
+        .hash(this.password, 10)
+        .then(hash => {
+            this.password = hash;
+
+            return next;
+        })
+        .catch(err => {
+            return next(err);
+        })
+});
+
+
+// Check if passwords match
+userSchema.method.validatePassword = function (passwordGuess) {
+  return bcrypt.compare(passwordGuess, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema, 'users');
